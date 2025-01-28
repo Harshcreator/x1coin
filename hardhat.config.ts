dfbe17b1767e4fc50acd211a5dcd45cf6618b0a6
Harshcreator <jayson22603@gmail.com>
2025-01-28 10:21:35 +0530

    add mochawesome reporter for test results and update test scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,18 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  mocha: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: './test-results',
+      reportFilename: 'x1coin-tests',
+      overwrite: true,
+      html: true,
+      json: true,
+      quiet: false,
+      timestamp: true
+    }
+  }
 };
 
 export default config;
