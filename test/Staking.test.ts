fb31ee76dc47783da80e89c8ec7fdbbf9bbbb66e
Harshcreator <jayson22603@gmail.com>
2025-01-28 10:11:14 +0530

    add logging to Staking and X1Coin tests for better traceability of balances and supply

--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -14,23 +14,30 @@ describe("Staking", () => {
 
     // Fund the staking contract with rewards 
     await token.transfer(await staking.getAddress(), ethers.parseUnits("1000", 18));
+    console.log("Staking Contract Balance:", ethers.formatUnits(await token.balanceOf(await staking.getAddress()), 18));
 
     // Fund user with tokens
     await token.transfer(user.address, ethers.parseUnits("1000", 18));
+    console.log("Initial User Balance:", ethers.formatUnits(await token.balanceOf(user.address), 18));
+
     await token.connect(user).approve(await staking.getAddress(), ethers.parseUnits("1000", 18));
 
     // Stake 100 tokens
     await staking.connect(user).stake(ethers.parseUnits("100", 18));
+    console.log("Staked Amount:", ethers.formatUnits(ethers.parseUnits("100", 18), 18));
+    console.log("User Balance After Stake:", ethers.formatUnits(await token.balanceOf(user.address), 18));
 
-    // Fast-forward 30 days
-    await network.provider.send("evm_increaseTime", [30 * 24 * 60 * 60]);
+    const THIRTY_DAYS = 30 * 24 * 60 * 60;
+    await network.provider.send("evm_increaseTime", [THIRTY_DAYS]);
     await network.provider.send("evm_mine");
+    console.log("Time elapsed: 30 days");
 
-    // Unstake
     await staking.connect(user).unstake();
+    const finalBalance = await token.balanceOf(user.address);
+    console.log("Final User Balance:", ethers.formatUnits(finalBalance, 18));
+    console.log("Total Rewards:", ethers.formatUnits(finalBalance - (ethers.parseUnits("1000", 18)), 18));
 
-    const balance = await token.balanceOf(user.address);
-    expect(balance).to.be.closeTo(
+    expect(finalBalance).to.be.closeTo(
       ethers.parseUnits("1000.8219", 18),
       ethers.parseUnits("0.1", 18)
     );
--- test/X1Coin.test.ts
+++ test/X1Coin.test.ts
@@ -6,6 +6,19 @@ describe("X1Coin", () => {
   it("Should deploy with 1B supply", async () => {
     const X1Coin = await ethers.getContractFactory("X1Coin");
     const token = await X1Coin.deploy();
-    expect(await token.totalSupply()).to.equal(ethers.parseUnits("1000000000", 18));
+    console.log("X1Coin deployed to:", await token.getAddress());
+
+    // Log token metadata
+    console.log("Token Name:", await token.name());
+    console.log("Token Symbol:", await token.symbol());
+    console.log("Token Decimals:", await token.decimals());
+
+    // Log supply info
+    const totalSupply = await token.totalSupply();
+    console.log("Total Supply (raw):", totalSupply.toString());
+    console.log("Total Supply (formatted):", ethers.formatUnits(totalSupply, 18));
+
+    // Verify supply
+    expect(totalSupply).to.equal(ethers.parseUnits("1000000000", 18));
   });
 });
